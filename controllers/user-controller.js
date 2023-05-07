1948116895ea8abc2e07cb45f2e2748cd6899576
nah <hef9874@gmail.com>
2023-05-07 16:03:52 -0400

    Add friend and delete friend added for user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,47 +1,114 @@
-const { User } = require ("../models");
+const { User } = require("../models");
 
 const userController = {
 
     async makeUser(req, res) {
-    try{
-        const newUser = await User.create(req.body);
-        res.status(200).json(newUser)
-    } catch(err) {
-        console.error(err)
-        res.status(500).json(err);
+        try {
+            const newUser = await User.create(req.body);
+            res.status(200).json(newUser)
+        } catch (err) {
+            console.error(err)
+            res.status(500).json(err);
         }
     },
 
     async allUsers(req, res) {
         try {
-        const allUsers = await User.find()
-        .populate({ path: 'thoughts' })
-        .populate({ path:'friends' })
+            const allUsers = await User.find()
+                .populate({ path: 'thoughts' })
+                .populate({ path: 'friends' })
 
-        res.status(200).json(allUsers);
-        
-    } catch(err) {
-        console.log(err);
-        res.status(500).json(err);
+            res.status(200).json(allUsers);
+
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
         }
     },
 
     async oneUser(req, res) {
-        try{
+        try {
             const userDb = await User.findById(req.params.id)
-            .populate({ path: 'thoughts' })
-            .populate({ path:'friends' });
+                .populate({ path: 'thoughts' })
+                .populate({ path: 'friends' });
 
             res.status(200).json(userDb);
-            if(!userDb) {
-                res.status(404).json({ message: 'no user with that Id'});
+            if (!userDb) {
+                res.status(404).json({ message: 'no user with that Id' });
                 return;
             }
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async updateUser(req, res) {
+        try {
+            const userDb = await User.findOneAndUpdate(
+                req.params.id, { $set: req.body }, { new: true },
+            );
+            if (!userDb) {
+                res.status(400).json({ message: 'No user exists with that Id' });
+                return;
+            }
+            res.status(200).json(userDb);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteUser(req, res) {
+        try {
+            const userDb = await User.findOneAndDelete({ _id: req.params.userId });
+            if (!userDb) {
+                res.status(404).json({ message: 'No user exists with that Id' });
+                return;
+            }
+            res.status(200).json(userDb);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        };
+    },
+
+    async addFriend(req, res) {
+        try {
+            const newFriend = await User.findByIdAndUpdate(
+                { _id: req.params.userId },
+                { $push: { friends: params.friendId } },
+                { new: true },
+            );
+            if (!newFriend) {
+                res.status(404).json({ message: 'No User found with that id.' });
+                return;
+            }
+            res.status(200).json(newFriend);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json(err);
+        }
+    },
+
+    async deleteFriend(req, res) {
+        try {
+            const userDb = await User.findByIdAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: params.friendId } },
+                { new: true },
+            );
+            if (!userDb) {
+                res.status(404).json({ message: "No user with that Id" });
+                return;
+            }
+            res.status(200).json(userDb);
         } catch(err) {
             console.error(err);
             res.status(500).json(err);
         }
-    }
+    },
+};
 
-}
+module.exports = userController;
 
