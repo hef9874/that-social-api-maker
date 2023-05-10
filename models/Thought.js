f3d473331d1b69ebce4b41d0442fb96466851c24
nah <hef9874@gmail.com>
2023-05-10 15:37:39 -0400

    Added models index and thought.js. Created main index.js

---
+++ models/Thought.js
@@ -0,0 +1,64 @@
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: () => moment(new Date()).format("MM/DD/YYYY"),
+        },
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    });
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        maxlength: 280,
+        minlength: 5
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: () => moment(new Date()).format("MM/DD/YYYY"),
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const thoughts = model('Thought', thoughtSchema);
+module.exports = thoughts;
